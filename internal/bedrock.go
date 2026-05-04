00bf82d8fba3fb399347eb2d0f1f011d35bf5ea9
Mateusz Biel <matbiel@gmail.com>
2026-05-04 21:17:36 +0200

    Fail if region not specified

--- internal/bedrock.go
+++ internal/bedrock.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"errors"
 
 	awscfg "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
@@ -26,6 +27,9 @@ func NewBedrockClient() (*BedrockClient, error) {
 	cfg, err := awscfg.LoadDefaultConfig(context.Background())
 	if err != nil {
 		return nil, err
+	}
+	if cfg.Region == "" {
+		return nil, errors.New("AWS Region not specified")
 	}
 	runtimeClient := bedrockruntime.NewFromConfig(cfg)
 	return &BedrockClient{
